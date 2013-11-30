91b6200c700aa498453c65a4b3766d4f92cf57de
root <root@croc.mimailserver.com>
2013-11-30 02:43:46 -0300

    Committer: Daniel Blanco Martin  On branch master
    	modified:   .gitignore
    	new file:   custom/extras/twwidget.php
    Twitter widget

---
+++ custom/extras/twwidget.php
@@ -0,0 +1,3 @@
+<a class="twitter-timeline" href="https://twitter.com/hdanielb" data-widget-id="330454219758899200">Tweets por @hdanielb</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
